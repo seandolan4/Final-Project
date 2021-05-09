769e319ec552747da52b127192176cb9ad86d7de
seandolan4 <83928728+seandolan4@users.noreply.github.com>
2021-05-09 19:22:49 -0400

    Add files via upload

---
+++ Finalproject/Finalapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from Finalapp.models import Studentdetails
+from Finalapp.models import Coursedetails
+
+# Register your models here.
+
+admin.site.register(Studentdetails)
+admin.site.register(Coursedetails)
